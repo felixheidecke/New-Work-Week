78be0499ed45735732a70554efc174f96da22400
Felix Heidecke <felix@heidecke.me>
2018-09-06 14:34:18 +0200

    Minor fixes on build and styles

--- gruntfile.js
+++ gruntfile.js
@@ -63,9 +63,9 @@ module.exports = function(grunt) {
 					}
 				},
 				files: {
-					'htdocs/index.html': [
-						'src/index.pug'
-					]
+					'htdocs/index.html':'src/index.pug',
+					'htdocs/impressum.html':'src/impressum.pug',
+					'htdocs/datenschutz.html':'src/datenschutz.pug'
 				}
 			}
 		},
